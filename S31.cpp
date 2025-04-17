8f82a4cb4aab89381ab3444984f2ad2ac39e7297
nikita <127011286+nikita142144@users.noreply.github.com>
2025-04-17 14:24:48 +0800

    Create S31.cpp

---
+++ S31.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(0,"Russian");
+    string numbers[10] {"Ноль", "Один", "Два", "Три", "Четыре", "Пять", "Шесть", "Семь", "Восемь", "Девять"};
+    int a;
+    cout << "Введите число:";
+    cin >> a;
+
+
+    for(int i=0;i<=10;i+=1){
+     if (a==i){
+        cout<<numbers[i]<<endl;
+    }
+
+    }
+
+  return 0;
+}
