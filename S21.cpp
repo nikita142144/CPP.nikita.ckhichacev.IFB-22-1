4f7a072718bb95986e739eb071895c0b4ecab91f
nikita <127011286+nikita142144@users.noreply.github.com>
2025-04-17 13:29:20 +0800

    Create S21.cpp

---
+++ S21.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    setlocale(0,"Russian");
+    cout << "Введите значение а:" << endl;
+    cin >> a;
+    cout << "Введите значение b:" << endl;
+    cin >> b;
+    ofstream fout;
+    fout.open("output.txt");
+    for(int i=a;i<=b;i+=1){
+
+    fout << i*3;
+
+    }
+    fout.close();
+  return 0;
+}
