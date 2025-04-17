aae6b09499f4c3b0d61e80389416fc55e884cec0
nikita <127011286+nikita142144@users.noreply.github.com>
2025-04-17 17:55:38 +0800

    Create M01.cpp

---
+++ M01.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    for(t = 1; t < 11; t += 1){
+        if(t == 11) break;
+        cout << t << '\t';
+    }
+    cout << endl;
+
+    for(t = 2; t < 21; t += 2){
+        if(t == 21) break;
+        cout << t << '\t';
+    }
+    cout << endl;
+
+    for(t = 3; t < 31; t += 3){
+        if(t == 31) break;
+        cout << t << '\t';
+    }
+    cout << endl;
+
+    for(t = 4; t < 41; t += 4){
+        if(t == 41) break;
+        cout << t << '\t';
+    }
+    cout << endl;
+
+    for(t = 5; t < 51; t += 5){
+        if(t == 51) break;
+        cout << t << '\t';
+    }
+    cout << endl;
+
+    for(t = 6; t < 61; t += 6){
+        if(t == 61) break;
+        cout << t << '\t';
+    }
+    cout << endl;
+
+    for(t = 7; t < 71; t += 7){
+        if(t == 71) break;
+        cout << t << '\t';
+    }
+    cout << endl;
+
+    for(t = 8; t < 81; t += 8){
+        if(t == 81) break;
+        cout << t << '\t';
+    }
+    cout << endl;
+
+    for(t = 9; t < 91; t += 9){
+        if(t == 91) break;
+        cout << t << '\t';
+    }
+    cout << endl;
+
+    for(t =10; t <101; t +=10){
+        if(t ==101) break;
+        cout << t << '\t';
+    }
+
+    return 0;
+}
