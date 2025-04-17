abc0921ce51282ec71d96b733feb3047160a5af0
nikita <127011286+nikita142144@users.noreply.github.com>
2025-04-17 16:46:16 +0800

    Update S12.cpp

--- S12.cpp
+++ S12.cpp
@@ -4,22 +4,27 @@ using namespace std;
 
 int main()
 {
-    int sum; int i;
-    while ( i<=1000){
-        if (i%7==0){
-            sum = sum+i;
+    int sum = 0;
+    int i = 1;
+
+    while (i <= 1000) {
+        if (i % 7 == 0) {
+            sum = sum + i;
         }
-       i=i+4;
-    };
+        i = i + 4;
+    }
     cout << sum << endl;
-     sum=0;
-     i = 1;
-     while ( i<=1000){
-        if (i%7==0){
-            sum = sum+i;
+
+    sum = 0;
+    i = 1;
+
+    while (i <= 1000) {
+        if (i % 7 == 0) {
+            sum = sum + i;
         }
-       i=i+4;
-    };
+        i = i + 4;
+    }
     cout << sum << endl;
+
     return 0;
 }
